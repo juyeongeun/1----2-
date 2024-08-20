256d0848e5814f8346e51f06ead6eda9dcc97a7b
jjuyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-08-20 16:18:28 +0900

    Revert "Feature/studyinfo, edit Study"

--- src/Main.js
+++ src/Main.js
@@ -1,23 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout.js";
-import StudyRoomPage from "./pages/StudyRoomPage.js";
-import CreateStudyPage from "./pages/CreateStudyPage.js";
-import HabitTrackerPage from "./pages/HabitTrackerPage.js";
-import FocusTimerPage from "./pages/FocusTimerPage.js";
-import StudyDetailPage from "./pages/StudyDetailPage.js";
-import EditStudyPage from "./pages/EditStudyPage.js";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout.js';
+import StudyRoomPage from './pages/StudyRoomPage.js';
+import CreateStudyPage from './pages/CreateStudyPage.js';
+import HabitTrackerPage from './pages/HabitTrackerPage.js';
+import FocusTimerPage from './pages/FocusTimerPage.js';
 
 function Main() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path='/' element={<Layout />}>
           <Route index element={<StudyRoomPage />} />
-          <Route path="/study" element={<CreateStudyPage />} />
-          <Route path="/study/:id" element={<StudyDetailPage />} />
-          <Route path="/editStudy" element={<EditStudyPage />} />
-          <Route path="/habit" element={<HabitTrackerPage />} />
-          <Route path="/focus" element={<FocusTimerPage />} />
+          <Route path='/study' element={<CreateStudyPage />} />
+          <Route path='/habit' element={<HabitTrackerPage />} />
+          <Route path='/focus' element={<FocusTimerPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
