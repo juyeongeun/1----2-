3d77fd30b2df799a3d2b9741b69c6d5ea3bfe17d
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2024-08-23 17:04:18 +0900

    add: localStorage를 활용한 데이터 저장 기능 추가

--- src/ListStudy/RecentStudies.js
+++ src/ListStudy/RecentStudies.js
@@ -1,59 +1,44 @@
 import styles from './RecentStudies.module.css';
 import { useState, useEffect } from 'react';
-import { getStudiesListId } from '../api/Studyhome.js';
+import useStudiesListId from '../hooks/useStudiesListId.js';
 import Study from './Study.js';
 
 function RecentStudies({ click }) {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [watched, setWatched] = useState([]);
 
-  // 상품 클릭 이벤트 핸들러
-  function onProductClick(click) {
-    const recentViews = JSON.parse(localStorage.getItem('recentViews')) || [];
-
-    // 기존 목록에서 해당 상품 제거
-    const filteredViews = recentViews.filter((id) => id !== click);
-
-    // 새로운 상품 ID 추가
-    filteredViews.unshift(click);
-
-    // 최근 본 상품 목록을 로컬스토리지에 저장
-    localStorage.setItem(
-      'recentViews',
-      JSON.stringify(filteredViews.slice(0, 3))
-    ); // 최근 10개 상품만 저장
-  }
+  const { recent, loading, error } = useStudiesListId(watched);
 
   useEffect(() => {
-    onProductClick(click);
-    const { data } = getStudiesListId(click);
-  }, [click]);
+    const watchedList = JSON.parse(localStorage.getItem('watched')) || [];
+    setWatched(watchedList);
+  }, []);
 
   useEffect(() => {
-    // 비동기 데이터 가져오기
-    async function fetchData() {
-      try {
-        const result = await getStudiesListId(click); // click을 매개변수로 전달
-        setData(result); // 데이터가 result.data 안에 있다고 가정
-        setLoading(false);
-      } catch (error) {
-        console.error('Failed to fetch studies:', error);
-        setError(error);
-        setLoading(false);
+    let watchedList = JSON.parse(localStorage.getItem('watched')) || [];
+
+    if (!watchedList.includes(click)) {
+      watchedList.unshift(click);
+      watchedList = Array.from(new Set(watchedList));
+
+      if (watchedList.length > 3) {
+        watchedList = watchedList.slice(0, 3);
       }
-    }
 
-    fetchData();
-  }, [click]); // click이 변경될 때마다 데이터 새로 가져오기
+      localStorage.setItem('watched', JSON.stringify(watchedList));
+      setWatched(watchedList);
+    }
+  }, [click]);
 
   return (
     <>
       <div className={styles.background}>
         <p className={styles.text}>최근 조회한 스터디</p>
         <div className={styles.head}>
-          <p>Product ID {click} has been clicked and added to recent views.</p>
-          {click ? <Study data={data} /> : <p>??</p>}
+          {watched.length > 1 ? (
+            <Study data={recent} />
+          ) : (
+            <p className={styles.nonStudy}>아직 조회한 스터디가 없어요</p>
+          )}
         </div>
       </div>
     </>
