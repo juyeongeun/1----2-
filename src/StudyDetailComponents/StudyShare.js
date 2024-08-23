d9780451b707cfc0878e809643aa1d2ca60d7f22
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-08-23 14:31:29 +0900

    chore: kakao공유 링크 로고 설정

--- src/StudyDetailComponents/StudyShare.js
+++ src/StudyDetailComponents/StudyShare.js
@@ -59,11 +59,6 @@ function StudyShare({ id, onShareClick }) {
           },
         },
       ],
-      profile: {
-        nickname: "studyForest",
-        thumbnailImageUrl:
-          "https://feature-share-kakao--zingy-faloodeh-281168.netlify.app/favicon.png", // 여기에 아이콘 URL을 넣으세요.
-      },
     });
     onShareClick();
   };
