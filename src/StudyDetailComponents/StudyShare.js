cd153a5ca32dcba236e75cbc09ad57f321a37f12
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-08-23 10:45:31 +0900

    feat: kakao sharing

--- src/StudyDetailComponents/StudyShare.js
+++ src/StudyDetailComponents/StudyShare.js
@@ -10,7 +10,7 @@ function StudyShare({ onShareClick }) {
   useEffect(() => {
     // .env 파일에서 환경 변수 가져오기
     const kakaoKey = process.env.REACT_APP_KAKAO_JAVASCRIPT_KEY;
-
+    console.log(kakaoKey);
     // 카카오 SDK 초기화
     if (kakaoKey && !Kakao.isInitialized()) {
       Kakao.init(kakaoKey);
