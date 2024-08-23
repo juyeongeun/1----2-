5d435dec56ad522250c582140658a620aeb5dcdd
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2024-08-23 17:06:23 +0900

    add: 스터디 둘러보기 api 추가

---
+++ src/hooks/useStudiesListId.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function useStudiesListId(ids) {
+  const [recent, setRecent] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  console.log(ids);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const responses = await Promise.all(
+          ids.map((id) =>
+            axios.get(`https://study-api-m36o.onrender.com/api/studies/${id}`)
+          )
+        );
+        setRecent(responses.map((response) => response.data));
+        setLoading(false);
+        // console.log(responses.data);
+        // console.log(ids);
+      } catch (err) {
+        setError(err);
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, [ids]);
+
+  return { recent, loading, error };
+}
+
+export default useStudiesListId;
