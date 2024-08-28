54c2fb92b0e82a40cda76c2a1d8b143d7f51f247
kangbeomjoon <kobe7698@gmail.com>
2024-08-28 23:50:13 +0900

    revised habitItem.js

--- src/components/TodayTodoList/HabitItem.js
+++ src/components/TodayTodoList/HabitItem.js
@@ -1,28 +1,20 @@
 import React from "react";
 import "./HabitItem.css";
-import ModalDelete from "../../img/btn_determinate.png";
 
 export default function HabitItem({ habit, handleChange, handleDelete }) {
   return (
     <div className="habit-item">
-      <div>
-        <input
-          className="habit-input" // 클래스 이름 추가
-          type="text"
-          value={habit.habitName}
-          onChange={(e) => handleChange(habit.habitId, e.target.value)}
-        />
-      </div>
-
-      <div>
-        <button className="plus-button" onClick={handleAddInput}>
-          <img
-            src={PlusButton}
-            alt="플러스 버튼"
-            style={{ width: "400px", height: "54px", marginRight: "5px" }}
-          />
-        </button>
-      </div>
+      <input
+        type="text"
+        value={habit.habitName}
+        onChange={(e) => handleChange(habit.habitId, e.target.value)}
+      />
+      <button
+        className="delete-button"
+        onClick={() => handleDelete(habit.habitId)}
+      >
+        삭제
+      </button>
     </div>
   );
 }
