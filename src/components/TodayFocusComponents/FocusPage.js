f3415a5fb3025866c7354644f6873a3a4a77ad62
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-08-29 14:25:02 +0900

    chore: 비밀번호 모달 위치 수정

--- src/components/TodayFocusComponents/FocusPage.js
+++ src/components/TodayFocusComponents/FocusPage.js
@@ -3,15 +3,37 @@ import useFetchStudy from "../../hooks/useFetchStudy.js";
 import FocusTimer from "./FocusTimer.js";
 import FocusTop from "./FocusTop.js";
 import "./FocusPage.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TimerSettingModal from "./TimerSettingModal.js";
+import PasswordModal from "../StudyDetailComponents/PasswordModal.js";
+import { useNavigate } from "react-router-dom";
 
 const FocusPage = () => {
   const { studyId } = useParams();
-  const { studyName, name, point, loading, error } = useFetchStudy(studyId);
-  const [modalOpen, setModalOpen] = useState(true);
+  const { studyName, name, point, password, loading, error } =
+    useFetchStudy(studyId);
+
+  const [isPasswordCorrect, setIsPasswordCorrect] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
   const [time, setTime] = useState(30 * 60);
   const [initTime, setInitTime] = useState(30 * 60);
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isPasswordCorrect) {
+      setIsModalOpen(false);
+      setModalOpen(true);
+    }
+  }, [isPasswordCorrect]);
+
+  const handleModalSubmit = () => {
+    setIsPasswordCorrect(true);
+  };
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    navigate(`/study/${studyId}`);
+  };
   return (
     <div className="main_Container">
       <FocusTop
@@ -28,7 +50,20 @@ const FocusPage = () => {
         studyId={studyId}
         point={point}
       />
-      {modalOpen ? (
+
+      {/* 비밀번호 모달 */}
+      <PasswordModal
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        onSubmit={handleModalSubmit}
+        correctPassword={password}
+        studyName={studyName}
+        name={name}
+        buttonText={"오늘의 집중으로 가기"}
+      />
+
+      {/* 타이머 설정 모달 */}
+      {modalOpen && isPasswordCorrect && (
         <TimerSettingModal
           setModalOpen={setModalOpen}
           name={name}
@@ -36,8 +71,6 @@ const FocusPage = () => {
           setTime={setTime}
           setInitTime={setInitTime}
         />
-      ) : (
-        <></>
       )}
     </div>
   );
