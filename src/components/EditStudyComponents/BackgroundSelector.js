68fcfd8a3e483f46eff2aac03d0568afd4e0f02b
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2024-08-30 14:57:10 +0900

    refactor: 수정하기 페이지 border 추가

--- src/components/EditStudyComponents/BackgroundSelector.js
+++ src/components/EditStudyComponents/BackgroundSelector.js
@@ -1,20 +1,29 @@
-import React from "react";
-import { backgrounds } from "../../img/ImgImport.js";
+import React from 'react';
+import { backgrounds } from '../../img/ImgImport.js';
 
 function BackgroundSelector({ selectedBackground, handleBackgroundClick }) {
   return (
-    <div className="formGroup">
+    <div className='formGroup'>
       <label>배경을 선택해 주세요</label>
-      <div className="backgroundOptions">
+      <div className='backgroundOptions'>
         {backgrounds.map((src, index) => (
           <div
             key={index}
             className={`backgroundOption ${
-              selectedBackground === index ? "selected" : ""
+              selectedBackground === index ? 'selected' : ''
             }`}
             onClick={() => handleBackgroundClick(index)}
             style={{ backgroundImage: `url(${src})` }}
-          ></div>
+          >
+            <div
+              key={index}
+              className={`backgroundOption ${
+                selectedBackground === index ? 'selectedIcon' : ''
+              }`}
+              onClick={() => handleBackgroundClick(index)}
+              style={{ backgroundImage: `url(${src})` }}
+            />
+          </div>
         ))}
       </div>
     </div>
